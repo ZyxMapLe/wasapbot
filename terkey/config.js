de57a999f4d6a9dc9d9cac4e19715e294ee72348
ZyxMapLe <m.rizkyramadam@gmail.com>
2021-08-03 01:10:08 +0700

    wasap

---
+++ terkey/config.js
@@ -0,0 +1,36 @@
+global.owner = ['62895378264836', '62895378264836'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = ['62895378264836'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '284d009ce74863ed',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'TH9T7qRgvtT6i3ir94pWGFe92oA',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+// Sticker WM
+global.packname = 'I hope you\'re fine'
+global.author = 'ZyxMapLe'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
